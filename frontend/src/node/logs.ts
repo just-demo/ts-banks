1806e3c6d7dc3161e0e73e5f66d2153325b40e84
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2026-06-20 17:15:11 +0300

    Remove unused code and simplify utils

--- frontend/src/node/logs.ts
+++ frontend/src/node/logs.ts
@@ -1,5 +1,3 @@
-import regex from './regex';
-
 export interface LogRequest {
     url: string;
     time: number;
@@ -7,12 +5,12 @@ export interface LogRequest {
 
 export default {
     parse(text: string): LogRequest[] {
-        return regex.findManyObjects<{url: string; time: string}>(text, /^GET (.*) (\d+)ms$/gm, {
-            url: 1,
-            time: 2
-        }).map(request => ({
-            url: request.url,
-            time: parseInt(request.time)
-        }));
+        const regex = /^GET (.*) (\d+)ms$/gm;
+        const requests: LogRequest[] = [];
+        let match;
+        while ((match = regex.exec(text))) {
+            requests.push({url: match[1], time: parseInt(match[2])});
+        }
+        return requests;
     }
 };
